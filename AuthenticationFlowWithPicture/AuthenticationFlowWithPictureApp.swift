12f56cbb84b854bfa4d700faa5edd1eef374c1bb
Berkay-Disli <berkaydisli.work@gmail.com>
2022-09-17 23:57:33 +0300

    Initial Commit

---
+++ AuthenticationFlowWithPicture/AuthenticationFlowWithPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationFlowWithPictureApp.swift
+//  AuthenticationFlowWithPicture
+//
+//  Created by Berkay Disli on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationFlowWithPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
